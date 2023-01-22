9d418d76af2c218664fff66244d83a2e8a90b258
Mykola Ostras <nikki.ice.promo@gmail.com>
2023-01-22 18:22:56 +0200

    Fix adaptive

--- js/nft.js
+++ js/nft.js
@@ -4,15 +4,19 @@
 /* 1 */
 /***/ (function() {
 
-const $HEADER = document.querySelector('.header__container');
-const $BURGER = $HEADER.querySelector('.nav-mobile__button');
-const $MENU = $HEADER.querySelector('.nav-mobile__list');
-const $BODY = document.querySelector('body');
+const $HEADER = document.querySelector('.header__container')
+const $BURGER = $HEADER.querySelector('.nav-mobile__button')
+const $MENU = $HEADER.querySelector('.nav-mobile__list')
+const $BODY = document.querySelector('body')
+
+console.log($HEADER.offsetHeight)
+
+$BODY.style.paddingTop = `${$HEADER.offsetHeight}px`
 
 $BURGER.onclick = () => {
-	$HEADER.classList.toggle('is-active');
+	$HEADER.classList.toggle('is-active')
 	$MENU.classList.toggle('is-active');
-	$BODY.classList.toggle('inactive');
+	$BODY.classList.toggle('inactive')
 }
 
 /***/ }),
